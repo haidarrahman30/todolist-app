7e206c1c2a3747a87ac6ad60bfc70f13a6239412
haidarrahman30 <haidarrahmans30@gmail.com>
2024-09-12 23:16:08 +0800

    fix: type localtime options

--- src/helper/utils.ts
+++ src/helper/utils.ts
@@ -1,11 +1,12 @@
 export const formatDate = (dateString: string) => {
-    const options: any = {
+    const options = {
         hour: '2-digit',
         minute: '2-digit',
         second: '2-digit',
         hour12: true,
     };
     const date = new Date(dateString);
+    // @ts-ignore
     return date.toLocaleTimeString('en-US', options);
 };
 
